30912ddbbaa4042c6b9a9d3fc1b32bb601d3d553
Eda M <edam@gmail.com>
2013-06-22 18:44:12 +0200

    (BY: eda) Adding field validations && validate Email function

--- app/models/user.php
+++ app/models/user.php
@@ -4,6 +4,16 @@ class UserModel extends AppModel {
 
 	var $name= "user";	
 
+	var $validates =array(
+				'first_name'=>array(NOT_NULL),
+				'last_name'=>array(NOT_NULL),
+				'email'=>array(NOT_NULL)
+				);
+
+	public function validEmail()
+	{
+		// function used to validate email address 
+	}
 }
 
 ?>
