9d4db53b0734f4bbb1fc9ea5def632ab7f0fe4b4
Veerpartap Singh <veerpartapsingh@gmail.com>
2013-06-23 20:14:12 +0200

    (BY: veer) payment gateway process details additions

--- app/controllers/components/payapl.php
+++ app/controllers/components/payapl.php
@@ -5,7 +5,11 @@ class PaypalComponent extends Component
 {
 
 	var $name ="paypal";
-
+	
+	public function setVars()	
+	{
+		// this function is used to set variables used in paypal payment gateway
+	}
 }
 
 
--- app/controllers/payments_controllers.php
+++ app/controllers/payments_controllers.php
@@ -5,7 +5,11 @@ class PaymentController extends AppController
 {
 
 	var $name = "payments";
-
+		
+	public function beforeFilter()
+	{
+		// this function gets executed before any action of the payment controller gets executed
+	}
 
 }
 
