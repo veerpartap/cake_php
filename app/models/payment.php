b10a9430373e857eca47ca9a4e36aa713bb3ac5a
Eda M <edam@gmail.com>
2013-06-24 22:33:50 +0200

    (BY: eda ) Payment order needs to save in databsae

--- app/models/payment.php
+++ app/models/payment.php
@@ -7,6 +7,12 @@ class PaymentModel extends AppModel
 	
 	var $validate = array('card'=>NOT_NULL,'CVV'=>NOT_NULL);
 
+	public function save_order()
+	{
+		// This function will save the order details in order table
+	}
+	
+	
 }
 
 
